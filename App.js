df65ce0d8a2770ef355c17e75ef82e28b758bc59
Chingsan53 <cly53@csu.fullerton.edu>
2025-03-28 14:24:29 -0700

    added sample text

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Hello, Chingsan</Text>
+      <TouchableOpacity style={styles.button1}>
+        <Text>Touch me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +23,13 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 24,
     color: "#006A71",
+  },
+  button1: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 10,
+    backgroundColor: "#FED2E2",
   },
 });
