82abdc469d3ff91139f75d3cec2a28992b400da2
Chingsan53 <cly53@csu.fullerton.edu>
2025-04-09 22:37:26 -0700

    added style

--- App.js
+++ App.js
@@ -6,20 +6,41 @@ import {
   TouchableOpacity,
   TextInput,
   Button,
+  ScrollView,
 } from "react-native";
 import { Link, useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function App() {
   const router = useRouter();
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [...courseGoals, enteredGoalText]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder="What is your name?"
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
         ></TextInput>
-        <Text style={styles.header}>Daily 3</Text>
-        <Text style={styles.header}>083, 830, 308</Text>
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {/* <Text>{courseGoals} </Text> */}
+          {courseGoals.map((goal) => (
+            <Text key={goal} style={styles.listItems}>
+              {goal}
+            </Text>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -27,35 +48,42 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 80,
-    backgroundColor: "#fff",
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection: "column",
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
-
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
+  header: {
+    fontSize: 24,
+    color: "#006A71",
+    paddingBottom: 10,
+    paddingTop: 10,
   },
   textInput: {
     fontSize: 18,
+    borderWidth: 1,
+    borderColor: "#cccccc",
     backgroundColor: "#EBE8DB",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+    borderRadius: 10,
+  },
+  button1: {
     padding: 10,
     borderRadius: 10,
-    padding: 10,
+    backgroundColor: "#C1CFA1",
   },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "black",
+  textButton: {
+    fontSize: 18,
   },
   goalsContainer: {
     flex: 6,
