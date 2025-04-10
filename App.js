8c00b71173a9900b31d1545a7a87fc5e4d90f422
Chingsan Ly <cly53@csu.fullerton.edu>
2025-04-09 18:31:02 -0700

    added something detail

--- App.js
+++ App.js
@@ -1,58 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-  Button,
-} from "react-native";
-import { Link, useRouter } from "expo-router";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  const router = useRouter();
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="What is your name?"
-        ></TextInput>
-        <Text style={styles.header}>Daily 3</Text>
-        <Text style={styles.header}>083, 830, 308</Text>
-      </View>
+      <Text style={styles.text}>Hello, Kimheang</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 80,
-    backgroundColor: "#fff",
-  },
-  inputContainer: {
-    flexDirection: "column",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  header: {
+
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    backgroundColor: "#9FB3DF",
+    borderRadius: 10,
+    padding: 10,
+  },
+  text: {
     fontSize: 24,
-    color: "#006A71",
-    paddingBottom: 10,
-    paddingTop: 10,
-  },
-  textInput: {
-    fontSize: 18,
-    backgroundColor: "#EBE8DB",
-    padding: 10,
-    borderRadius: 10,
-  },
-  button1: {
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: "#C1CFA1",
-  },
-  textButton: {
-    fontSize: 18,
+    fontWeight: "bold",
+    color: "black",
   },
 });
